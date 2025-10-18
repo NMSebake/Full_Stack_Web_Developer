ce2ffbb34c7ca9bfc901d9acbcfd1c16cad3363e
NMSebake <sebaketsime@gmail.com>
2025-10-18 21:37:55 +0200

    new folder for javascript

--- javascript-exercises/06_repeatString/repeatString.js
+++ javascript-exercises/06_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    string.repeat(num)
 };
 
+repeatString('hey', 3)
 // Do not edit below this line
 module.exports = repeatString;
